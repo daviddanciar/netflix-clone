3918eb86b65f67eb5ef2afd84656790ab1a86ee0
Danciar David <danciar.david@gmail.com>
2024-07-16 12:35:26 +0300

    clean-up

--- src/db/firebase.ts
+++ src/db/firebase.ts
@@ -3,18 +3,6 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 require('dotenv').config();
 
-
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyArpjlW0ytmPTkE6pU3Hd_rUFHeczi3IzE",
-//   authDomain: "netflix-project-51552.firebaseapp.com",
-//   projectId: "netflix-project-51552",
-//   storageBucket: "netflix-project-51552.appspot.com",
-//   messagingSenderId: "897584119995",
-//   appId: "1:897584119995:web:9215073a5ea102f97fa99e"
-// };
-
-
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY_FIREBASE,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
@@ -23,7 +11,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
 };
-console.log(firebaseConfig,process.env.PUBLIC_API_KEY_FIREBASE)
+
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
